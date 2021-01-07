dae1e5d9031f244f95c88277738c905dfcfd12df
Ben Spotswood <spotswoodb@gmail.com>
2021-01-07 15:34:16 -0600

    updated menu

--- lib/podcast/menu.rb
+++ lib/podcast/menu.rb
@@ -19,9 +19,17 @@ class Menu
         puts "Dive deep into a list of the top podcasts!"
     end
 
-    def self.display(category)
+    def self.display_category(category)
         self.print_all(category)
         input_prompt = "For a top podcast list in a category you want, select a #{category}'s number"
         puts input_prompt
+    end
+
+    def self.display_title(title)
+
+    end
+
+    def self.display_description(description)
+        
     end
 end
